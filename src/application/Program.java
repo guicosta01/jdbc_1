7c7069c7277117d6775e77c46688b4aa25609906
Guilherme Costa <guiferreiracosta@hotmail.com>
2023-06-24 00:31:13 -0300

    delete and trans model

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,106 @@ import java.text.SimpleDateFormat;
 
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		transaction_data();
+	}
 	
+	public static void transaction_data() {
+		Connection conn = null;
+		Statement st = null; 
+		
+		try {
+			conn = DB.getConnection();
+			
+			conn.setAutoCommit(false); //nao 'salva' -> transacao
+			
+			st = conn.createStatement();
+			
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId=1");
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId=2");
+			
+			conn.commit(); // caso nao ocorra erros
+			
+			System.out.println("row1: " + rows1);
+			System.out.println("row2: " + rows2);
+
+			
+			}	
+			catch (SQLException e) {
+				try {
+					conn.rollback();
+					e.getMessage();
+					//throw new DbException("Error by: " + e.getMessage());
+				} catch (SQLException e1) {
+					//throw new DbException("Error rollback by: " + e1.getMessage());
+					e1.getMessage();
+				} 
+			}
+			finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+			}
+	}
+	
+	public static void delete_data() {
+		Connection conn = null;
+		PreparedStatement st = null; 
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+			st.setInt(1, 5); //deparmento 2 vai aumentar 200
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows: "+ rowsAffected);
+			
+			}	
+			catch (SQLException e) {
+				throw new DbIntegrityException(e.getMessage());
+			}
+			finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+			}
+		
+	}
+	
+	public static void update_data() {
+		Connection conn = null;
+		PreparedStatement st = null; 
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? WHERE DepartmentId = ?");
+
+			st.setDouble(1, 200.0); //soma 200 no salario
+			st.setInt(2, 2); //deparmento 2 vai aumentar 200
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows: "+ rowsAffected);
+			
+			}	
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+			}
+	}
+	
+	public static void write_data() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Connection conn = null; 
 		PreparedStatement st = null; 
 		
@@ -30,11 +123,11 @@ public class Program {
 					+ "(?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
 			
-			st.setString(1, "Guilherme"); //replace nos "?"
-			st.setString(2, "guilherme@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("09/11/2001").getTime()));
+			st.setString(1, "Bruna"); //replace nos "?"
+			st.setString(2, "bruna@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("09/11/2002").getTime()));
 			st.setDouble(4, 25000.0);
-			st.setInt(5, 4);
+			st.setInt(5, 3);
 			
 			int rowsAffected = st.executeUpdate();
 			
@@ -59,7 +152,6 @@ public class Program {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
 	}
 	
 	public static void get_data() {
